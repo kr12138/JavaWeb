1d4f449153c16631ee785a0be4850225df8b0743
kr12138 <37018937+kr12138@users.noreply.github.com>
2019-12-21 13:22:24 +0800

    Revert "teacher Table"
    This reverts commit ad65648a98f1db36b8132c66b81896665c4db82e.

--- src/main/java/com/lyon/Repository/TeacherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lyon.Repository;
-
-import com.lyon.Entity.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
-
-}
